1e87325de3a6f7d20287e589faedbc67cc675a39
Steinar Bang <sb@dod.no>
2015-08-31 19:33:41 +0200

    Added column headers and row numbers, and corrected the column order.

--- calibre_plugin/model.py
+++ calibre_plugin/model.py
@@ -8,6 +8,15 @@ class OpdsBooksModel(QAbstractTableModel):
         QAbstractTableModel.__init__(self, parent)
         self.books = books
 
+    def headerData(self, section, orientation, role):
+        if role != Qt.DisplayRole:
+            return None
+        if orientation == Qt.Vertical:
+            return section + 1
+        if section >= len(self.column_headers):
+            return None
+        return self.column_headers[section]
+
     def rowCount(self, parent):
         return len(self.books)
 
@@ -23,4 +32,4 @@ class OpdsBooksModel(QAbstractTableModel):
         opdsBook = self.books[row]
         if col >= len(self.column_keys):
             return None
-        return opdsBook[self.column_keys[col - 1]]
+        return opdsBook[self.column_keys[col]]
