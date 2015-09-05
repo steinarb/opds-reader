9625db7c461184150374cb082aa81dcea1a4b838
Steinar Bang <sb@dod.no>
2015-09-05 12:03:00 +0200

    Experimenting with feed attribute names, also trying to find the "next" link of a feed.

--- experiments/readcalibreopds.py
+++ experiments/readcalibreopds.py
@@ -1,12 +1,19 @@
 import feedparser
 
-calibre_url = "http://localhost:8080/opds"
+calibre_url = "http://edwards.hjemme.lan:8080/opds"
 
 feed = feedparser.parse(calibre_url)
 
 print feed.keys()
-newest_url = feed['entries'][0]['links'][0]['href']
+newest_url = feed.entries[0].links[0].href
 
 newest_feed = feedparser.parse(newest_url)
 
-print newest_feed['entries'][5]['links']
+print newest_feed.keys()
+newest_feed_feed_links = newest_feed.feed.links
+print newest_feed_feed_links
+next_link = newest_feed_feed_links[len(newest_feed_feed_links) - 1]
+print next_link
+#print newest_feed.entries[9].keys()
+#print newest_feed.entries[8]
+#print newest_feed.entries[0].links
