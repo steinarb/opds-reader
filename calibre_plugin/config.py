e9bf2466d510f9c9805df520a38f568fe35fcbe9
Steinar Bang <sb@dod.no>
2015-09-07 20:20:33 +0200

    Full name for the layout object.

--- calibre_plugin/config.py
+++ calibre_plugin/config.py
@@ -12,20 +12,22 @@ from calibre.utils.config import JSONConfig
 prefs = JSONConfig('plugins/opds_client')
 
 prefs.defaults['opds_url'] = 'http://localhost:8080/opds'
+prefs.defaults['hideNewspapers'] = True
+prefs.defaults['hideBooksAlreadyInLibrary'] = True
 
 class ConfigWidget(QWidget):
 
     def __init__(self):
         QWidget.__init__(self)
-        self.l = QHBoxLayout()
-        self.setLayout(self.l)
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
 
         self.label = QLabel('OPDS URL: ')
-        self.l.addWidget(self.label)
+        self.layout.addWidget(self.label)
 
         self.opds_url = QLineEdit(self)
         self.opds_url.setText(prefs['opds_url'])
-        self.l.addWidget(self.opds_url)
+        self.layout.addWidget(self.opds_url)
         self.label.setBuddy(self.opds_url)
 
     def save_settings(self):
